24d8bb544ad4f8d265fa3f93b0a6e09338836e9a
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2023-06-09 00:54:19 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,22 @@
       </header>
       <div class="container cards-container">
         <div class="card">
-          <p class="main-text"></p>
-          <p class="date"></p>
+          <div class="card-body">
+            <h5 class="card-title">New Note - 1</h5>
+            <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, officia.</p>
+          </div>
+          <div class="card-footer">
+            <p class="date">15/15/1550</p>
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">New Note - 2</h5>
+            <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, officia.</p>
+          </div>
+          <div class="card-footer">
+            <p class="date">16/15/1550</p>
+          </div>
         </div>
       </div>
     </div>
@@ -42,6 +56,30 @@
     height: 50px;
     padding: 10px;
     cursor: pointer;
+    font-size: 1.5rem;
+    line-height: 0;
+  }
 
+  .cards-container{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .card{
+    width: 225px;
+    height: 225px;
+    background-color: orange;
+    padding: 10px;
+    border-radius: 15px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+
+  .card .card-footer{
+    font-size: 0.8rem;
+    font-weight: bold;
+    border-top: unset;
+    background-color: unset;
+    text-align: start;
+    padding-left: 10px;
   }
 </style>
