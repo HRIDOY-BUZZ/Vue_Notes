03c67889d2eee2f7d6807f4be90fec3628c45b91
Al-Amin Islam Hridoy <36563890+HRIDOY-BUZZ@users.noreply.github.com>
2023-06-09 16:40:52 +0600

    note add and show done

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,14 @@
       date: new Date(),
       color: getRandomColor()
     })
+    noteTitle.value = "";
+    noteBody.value = "";
   }
 </script>
 <template>
   <main>
     <div class="container">
-      <header>
+      <header class="container">
         <h1>Notes</h1>
         <button type="button" class="add" data-bs-toggle="modal" data-bs-target="#addModal">+</button>
         <!-- MODAL -->
@@ -51,107 +53,17 @@
         </div>
         <!-- MODAL END -->
       </header>
-      {{ notes }}
       <div class="container cards-container">
-        <button class="card">
+        <button v-for="note in notes" class="card" :style="{backgroundColor: note.color}">
           <div class="card-body">
-            <h5 class="card-title">New Note - 1</h5>
-            <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, officia.</p>
+            <h5 class="card-title">{{ note.title }}</h5>
+            <p class="main-text">{{ note.body }}</p>
           </div>
           <div class="card-footer">
-            <p class="date">15/15/1550</p>
-          </div>
-        </button>
-        <button class="card">
-          <div class="card-body">
-            <h5 class="card-title">New Note - 2</h5>
-            <p class="main-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, officia.</p>
-          </div>
-          <div class="card-footer">
-            <p class="date">16/15/1550</p>
+            <p class="date">{{ note.date.toLocaleDateString("en-Us") }}</p>
           </div>
         </button>
       </div>
     </div>
   </main>
 </template>
-
-<style scoped>
-  main{
-    height: 100vh;
-    width: 100vw;
-  }
-
-  header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  h1{
-    font-weight: bold;
-    margin-bottom: 25px;
-    font-size: 75px;
-  }
-
-  button.add{
-    border: none;
-    border-radius: 100%;
-    background-color: black;
-    color: white;
-    width: 50px;
-    height: 50px;
-    padding: 10px;
-    cursor: pointer;
-    font-size: 1.5rem;
-    line-height: 0;
-  }
-  /* MODAL */
-  .modal-header{
-    padding: 10px;
-  }
-  .modal-header h1{
-    margin-bottom: 0;
-  }
-
-  .modal-footer{
-    justify-content: space-between;
-    padding: 10px;
-  }
-
-  .modal-footer button{
-    font-size: 0.8rem;
-    margin: 0;
-  }
-  /* MODAL END */
-  /* CARD */
-  .cards-container{
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .cards-container a{
-    text-decoration: none;
-  }
-  .card{
-    width: 225px;
-    height: 225px;
-    background-color: orange;
-    padding: 10px;
-    border-radius: 15px;
-    margin-right: 20px;
-    margin-bottom: 20px;
-    text-align: start;
-  }
-
-  .card .card-footer{
-    font-size: 0.8rem;
-    font-weight: bold;
-    border-top: unset;
-    background-color: unset;
-    text-align: start;
-    padding-left: 10px;
-  }
-  /* CARD END */
-
-</style>
